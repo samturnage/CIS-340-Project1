828f6e602317120ee4ef3ded8ad167081b80a0df
Derek Morey <demorey@rodin.cba.csuohio.edu>
2015-02-03 20:13:32 -0500

    started quicksort implementation

--- node.c
+++ node.c
@@ -4,7 +4,33 @@
 
 struct mynode* quicksort(struct mynode *head)
 {
-    return head;
+	int pivot = head->value;
+	struct mynode *current, *left, *right, *l_current, *r_current;
+	left = (struct mynode *)malloc(sizeof(struct mynode));
+	right = (struct mynode *)malloc(sizeof(struct mynode));
+	l_current = left;
+	r_current = right;
+
+	for (current=head; current; current=current->next) {
+		if (current->value < pivot) {
+			l_current->value = current->value;
+			l_current->next = (struct mynode *)malloc(sizeof(struct mynode));
+			l_current = l_current->next;
+		} else {
+			r_current->value = current->value;
+			r_current->next = (struct mynode *)malloc(sizeof(struct mynode));
+			r_current = r_current->next;
+		}
+	}
+
+	left = quicksort(left);
+	right = quicksort(right);
+
+	for (current=left; current; current=current->next) {	
+	}
+
+	current->next = right;
+    return left;
 }
 
 void printlist(struct mynode *head)
