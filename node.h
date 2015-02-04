d3895eb41176774e0d0f6a45714eaca57464686f
samturnage <samturnage4@yahoo.com>
2015-02-04 12:24:57 -0500

    added comments to node.h

--- node.h
+++ node.h
@@ -1,9 +1,12 @@
+//mynode struct
+//header for a node struct whitch holds a single integer and two pointers to other nodes
+//element of a doubly linked list of type int
 struct mynode {
     int value;
     struct mynode *next;
     struct mynode *prev;
 };
-
+//declare methods to sort, print, and free the linked list
 struct mynode* quicksort(struct mynode *);
 void printlist(struct mynode *);
 void freelist(struct mynode *);
